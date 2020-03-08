fc93a5a497128c5f1a39e78cdb1c4d02c4f7e109
Łukasz Niedziałek <intergenixyt@gmail.com>
2020-03-08 02:55:36 +0100

    Add contact page.

--- lib/ui/contact/contact_page.dart
+++ lib/ui/contact/contact_page.dart
@@ -1,11 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:portfolio2/themes/text_styles.dart';
 
 class ContactPage extends StatelessWidget {
+  // todo 'do you need help, have a job offer, or wanna go for beer?
+  // what ever it is dont hesitate to email me
   @override
   Widget build(BuildContext context) {
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Text('CONTACT PAGE'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 64,
+            width: w,
+            color: Colors.amber,
+            child: _buildTopBar(),
+          ),
+          _buildBody(context, w, h - 64),
+        ],
+      ),
     );
   }
 
+  Widget _buildTopBar() {
+    // todo add logic with navigation
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'HOME',
+          style: headerText,
+          maxLines: 1,
+        ),
+        SizedBox(width: 32),
+        Text(
+          'PORTFOLIO',
+          style: headerText,
+          maxLines: 1,
+        ),
+        SizedBox(width: 32),
+        Text(
+          'RESUME',
+          style: headerText,
+          maxLines: 1,
+        ),
+        SizedBox(width: 32),
+        Text(
+          'CONTACT',
+          style: headerText.copyWith(fontSize: 20),
+          maxLines: 1,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBody(BuildContext context, double w, double h) {
+    return Container(
+      width: w,
+      height: h,
+      child: Stack(
+        children: [
+          Container(
+            width: w,
+            height: h,
+            foregroundDecoration: BoxDecoration(
+              color: Colors.black87,
+            ),
+            child: Image.asset(
+              'assets/mailbox.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            width: w,
+            height: h,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'CONTACT ME',
+                  style: headerText.copyWith(
+                    color: Colors.white,
+                    fontSize: 48,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'If you have any questions or offers,',
+                  style: normalText.copyWith(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  'please feel free to write.',
+                  style: normalText.copyWith(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(height: 32),
+                Container(
+                  color: Colors.amber[600],
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
+                    child: Text(
+                      'lhniedzialek@gmail.com',
+                      style: normalText.copyWith(fontSize: 20, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  splashColor: Colors.white,
+                  onPressed: () async {
+                    await Clipboard.setData(
+                      ClipboardData(text: "lhniedzialek@gmail.com"),
+                    );
+                  },
+                  child: Text(
+                    'COPY TO CLIPBOARD',
+                    style: headerText.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
