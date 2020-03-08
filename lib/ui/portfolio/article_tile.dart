9d9299819b8889917902f4273d56d2b12ff87584
Łukasz Niedziałek <intergenixyt@gmail.com>
2020-03-08 22:13:36 +0100

    Add basic portfolio page.

--- lib/ui/portfolio/article_tile.dart
+++ lib/ui/portfolio/article_tile.dart
@@ -1,55 +1,72 @@
 import 'package:flutter/material.dart';
 
 class ArticleTile extends StatelessWidget {
-	final Widget child;
-	final Function() onTap;
-	
-	ArticleTile({
-		@required this.child,
-		this.onTap,
-	});
-	
-	Widget build(BuildContext context) {
-		return GestureDetector(
-			onTap: onTap,
-			child: ClipPath(
-				clipper: ArticleTileClip(),
-				child: Container(
-					width: 200,
-					height: 300,
-					color: Colors.amber,
-					child: Center(child: child),
-				),
-			),
-		);
-	}
+  static double innerHeight = 300;
+  static double innerWidth = 200;
+  static double outerHeight = 312;
+  static double outerWidth = 212;
+
+  final Widget child;
+  final Function() onTap;
+
+  ArticleTile({
+    @required this.child,
+    this.onTap,
+  });
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipPath(
+        clipper: ArticleTileClip(16),
+        child: Container(
+          width: outerWidth,
+          height: outerHeight,
+          color: Colors.amber,
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: ClipPath(
+              clipper: ArticleTileClip(12),
+              child: SizedBox(
+                width: innerWidth,
+                height: innerHeight,
+                child: Center(child: child),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class ArticleTileClip extends CustomClipper<Path> {
-	@override
-	Path getClip(Size size) {
-		Path path = Path();
-		
-		final w = size.width;
-		final h = size.height;
-		
-		final double cutSize = 16;
-		
-		path.moveTo(cutSize, 0);
-		path.lineTo(w - cutSize, 0);
-		path.lineTo(w, cutSize);
-		path.lineTo(w, h - cutSize);
-		path.lineTo(w - cutSize, h);
-		path.lineTo(cutSize, h);
-		path.lineTo(0, h - cutSize);
-		path.lineTo(0, cutSize);
-		path.close();
-		
-		return path;
-	}
-	
-	@override
-	bool shouldReclip(CustomClipper<Path> oldClipper) {
-		return true;
-	}
+  final double cutSize;
+
+  ArticleTileClip(this.cutSize);
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    final w = size.width;
+    final h = size.height;
+
+    path.moveTo(cutSize, 0);
+    path.lineTo(w - cutSize, 0);
+    path.lineTo(w, cutSize);
+    path.lineTo(w, h - cutSize);
+    path.lineTo(w - cutSize, h);
+    path.lineTo(cutSize, h);
+    path.lineTo(0, h - cutSize);
+    path.lineTo(0, cutSize);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
 }
--- lib/ui/portfolio/portfolio_page.dart
+++ lib/ui/portfolio/portfolio_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio2/themes/text_styles.dart';
+import 'package:portfolio2/ui/common/box/box.dart';
+import 'package:portfolio2/ui/common/box/no_color_scroll_behaviour.dart';
 import 'package:portfolio2/ui/portfolio/article_tile.dart';
 import 'package:portfolio2/ui/top_bar/top_bar.dart';
 
@@ -8,22 +11,307 @@ class PortfolioPage extends StatelessWidget {
     final w = MediaQuery.of(context).size.width;
     final h = MediaQuery.of(context).size.height - TopBar.height;
 
+    //todo add minimal h (tile + padding)
+    
+
     return Scaffold(
       body: Column(
         children: <Widget>[
           TopBar(),
+          _buildUpperPart(w, h / 2),
+          _buildLowerPart(w, h / 2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUpperPart(double w, double h) {
+    return Container(
+      width: w,
+      height: h,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/mountains_2.jpg',
+            fit: BoxFit.cover,
+          ),
           Container(
-            width: w,
-            height: h,
             color: Colors.black87,
+          ),
+          Align(
+            alignment: Alignment.center,
             child: Padding(
               padding: const EdgeInsets.all(16),
-              child: Column(
-                children: <Widget>[
-                  ArticleTile(
-                    child: Text('A'),
-                  ),
-                ],
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 800),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'DEVELOPED',
+                            style: headerText.copyWith(
+                              color: Colors.white,
+                              fontSize: 32,
+                            ),
+                          ),
+                          Text(
+                            'APPS',
+                            style: headerText.copyWith(
+                              color: Colors.white,
+                              fontSize: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 32),
+                    Flexible(
+                      child: ScrollConfiguration(
+                        behavior: NoColorScrollBehavior(),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: <Widget>[
+                              ArticleTile(
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    Image.asset(
+                                      'assets/placelytics/logo.jpg',
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Container(color: Colors.black54),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Container(
+                                          width: ArticleTile.innerWidth,
+                                          height: ArticleTile.innerHeight / 2,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'Placelytics',
+                                              style: normalText.copyWith(
+                                                color: Colors.white,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 16),
+                              ArticleTile(
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    Image.asset(
+                                      'assets/thg/logo.png',
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Container(color: Colors.black54),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Container(
+                                          width: ArticleTile.innerWidth,
+                                          height: ArticleTile.innerHeight / 2,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'The Hardest Game',
+                                              style: normalText.copyWith(
+                                                color: Colors.white,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 16),
+                              ArticleTile(
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    Image.asset(
+                                      'assets/pictile/logo.png',
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Container(color: Colors.black54),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Container(
+                                          width: ArticleTile.innerWidth,
+                                          height: ArticleTile.innerHeight / 2,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'Pictile',
+                                              style: normalText.copyWith(
+                                                color: Colors.white,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  
+  Widget _buildLowerPart(double w, double h) {
+    return Container(
+      width: w,
+      height: h,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/mountains_1.jpg',
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black87,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 800),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'OTHER',
+                            style: headerText.copyWith(
+                              color: Colors.white,
+                              fontSize: 32,
+                            ),
+                          ),
+                          Text(
+                            'ARTICLES',
+                            style: headerText.copyWith(
+                              color: Colors.white,
+                              fontSize: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 32),
+                    Flexible(
+                      child: ScrollConfiguration(
+                        behavior: NoColorScrollBehavior(),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: <Widget>[
+                              ArticleTile(
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    Image.asset(
+                                      'assets/notes.jpg',
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Container(color: Colors.black54),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Container(
+                                          width: ArticleTile.innerWidth,
+                                          height: ArticleTile.innerHeight / 2,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'Algorithms',
+                                              style: normalText.copyWith(
+                                                color: Colors.white,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 16),
+                              ArticleTile(
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    Image.asset(
+                                      'assets/squares.jpg',
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Container(color: Colors.black54),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Container(
+                                          width: ArticleTile.innerWidth,
+                                          height: ArticleTile.innerHeight / 2,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'Animations',
+                                              style: normalText.copyWith(
+                                                color: Colors.white,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
