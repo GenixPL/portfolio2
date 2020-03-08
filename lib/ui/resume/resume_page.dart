8a98189f716bd25dbc6ab06e81555018abc889c6
Łukasz Niedziałek <intergenixyt@gmail.com>
2020-03-09 00:31:02 +0100

    Add ScrollBar

--- lib/ui/resume/resume_page.dart
+++ lib/ui/resume/resume_page.dart
@@ -30,30 +30,32 @@ class ResumePage extends StatelessWidget {
   Widget _buildBody() {
     return ScrollConfiguration(
       behavior: NoColorScrollBehavior(),
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              ResumeSummary(),
-              SizedBox(width: 16),
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 600),
-                child: Column(
-                  children: <Widget>[
-                    ResumeSkills(),
-                    SizedBox(height: 24),
-                    ResumeEducation(),
-                    _buildSeparator(),
-                    ResumeWorkExperience(),
-                    _buildSeparator(),
-                    ResumeProjects(),
-                  ],
+      child: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ResumeSummary(),
+                SizedBox(width: 16),
+                ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 600),
+                  child: Column(
+                    children: <Widget>[
+                      ResumeSkills(),
+                      SizedBox(height: 24),
+                      ResumeEducation(),
+                      _buildSeparator(),
+                      ResumeWorkExperience(),
+                      _buildSeparator(),
+                      ResumeProjects(),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
